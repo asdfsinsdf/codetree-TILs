5c9d08688601594ceb610dadcc95b3e4b3632ab1
asdfsinsdf <152978780+asdfsinsdf@users.noreply.github.com>
2024-06-23 19:06:18 +0900

    Title: 간단한 사칙연산, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

--- 240623/간단한 사칙연산/simple-arithmetic-operation.java
+++ 240623/간단한 사칙연산/simple-arithmetic-operation.java
@@ -1,12 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-        int a = 2, b = 5;
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
         System.out.println(a + b);
         System.out.println(a - b);
         System.out.println(a / b);
         System.out.println(a % b);
-
-
     }
 }
