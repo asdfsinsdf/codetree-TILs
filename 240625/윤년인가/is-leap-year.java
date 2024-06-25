d64035fb2c41f6a60665bd66cc4798e92c8b34c8
woong <152978780+asdfsinsdf@users.noreply.github.com>
2024-06-25 19:39:02 +0900

    Title: 윤년인가, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240625/윤년인가/is-leap-year.java
+++ 240625/윤년인가/is-leap-year.java
@@ -4,14 +4,16 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int y = sc.nextInt();
-
+        
         if(y % 4 == 0){
-            System.out.println("true");
-             
+            if(y % 100 == 0 && y % 400 !=0){
+                System.out.println("false");
+            }else{
+                System.out.println("true");
+            }
+            
         }else{
             System.out.println("false");
         }
-
-
     }
 }
