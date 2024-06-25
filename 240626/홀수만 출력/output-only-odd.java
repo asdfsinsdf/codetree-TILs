509c2999f2e1e2cc97d315ec7102cfdf72cee89d
woong <152978780+asdfsinsdf@users.noreply.github.com>
2024-06-26 00:16:51 +0900

    Title: 홀수만 출력, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240626/홀수만 출력/output-only-odd.java
+++ 240626/홀수만 출력/output-only-odd.java
@@ -6,7 +6,7 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        for(int i = 1; a <= i && b >= i; i++){
+        for(int i = a; b >= i; i++){
             if(i % 2 == 1){
                 System.out.print(i +" ");    
             }
