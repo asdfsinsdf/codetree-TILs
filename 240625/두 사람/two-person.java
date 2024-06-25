f7a42ee2bb68294fb68682e7a063aeed2cca553b
woong <152978780+asdfsinsdf@users.noreply.github.com>
2024-06-25 18:31:14 +0900

    Title: 두 사람, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240625/두 사람/two-person.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        
+        int a = sc.nextInt();
+        char s = sc.next().charAt(0);
+        
+        
+        int b = sc.nextInt();
+        char s_1 = sc.next().charAt(0);
+
+       
+        if (a >= 19 && s == 'M') {
+            System.out.println("1");
+        } else if (b >= 19 && s_1 == 'M') {
+            System.out.println("0");
+        } else {
+            System.out.println("0");
+        }
+
+        
+    }
+}
