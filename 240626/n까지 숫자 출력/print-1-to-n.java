aefe0bcfa11798ac325856912341bba390e648ff
woong <152978780+asdfsinsdf@users.noreply.github.com>
2024-06-26 18:12:56 +0900

    Title: n까지 숫자 출력, Time: 166ms, Memory: 10MB, Status: Passed - CodeTree

--- 240626/n까지 숫자 출력/print-1-to-n.java
+++ 240626/n까지 숫자 출력/print-1-to-n.java
@@ -3,10 +3,11 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int n = 1;
-        while(n<11){
-            System.out.print(n + " ");
-            n++;
+        int n = sc.nextInt();
+        int i = 1;
+        while(i<=n){
+            System.out.print(i + " ");
+            i++;
         }
 
         
